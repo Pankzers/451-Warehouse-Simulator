2b53153007c194a28c159afe8325ec30aedfd399
DESKTOP-3C06VN5\pckno <pcknoblich@gmail.com>
2020-12-06 17:53:36 -0800

    Early Driving controls

---
+++ Assets/Scripts/DriveForklift.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveForklift : MonoBehaviour
+{
+    public GameObject forkliftBase;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Quaternion q = Quaternion.AngleAxis(-0.1f, Vector3.up);
+            forkliftBase.transform.localRotation = q * forkliftBase.transform.localRotation;
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            forkliftBase.transform.localPosition += forkliftBase.transform.right * -0.01f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            Quaternion q = Quaternion.AngleAxis(0.1f, Vector3.up);
+            forkliftBase.transform.localRotation = q * forkliftBase.transform.localRotation;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            forkliftBase.transform.localPosition += forkliftBase.transform.right * 0.01f;
+        }
+    }
+}
---
+++ Assets/Scripts/FrontEndManipulation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontEndManipulation : MonoBehaviour
+{
+    public GameObject leftFork, rightFork;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit[] hits = Physics.RaycastAll(ray);
+            for (int i = 0; i < hits.Length; i++)
+            {
+                Debug.Log(hits[i].transform.name);
+            }
+        }
+    }
+}
