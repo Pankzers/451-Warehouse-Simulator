c620ec5c76ef7441bd9510f3cdaf87fec2445668
DESKTOP-3C06VN5\pckno <pcknoblich@gmail.com>
2020-12-11 11:16:48 -0800

    Actual shelf collision implemented by storing old position

--- Assets/Scripts/DriveForklift.cs
+++ Assets/Scripts/DriveForklift.cs
@@ -6,7 +6,6 @@ public class DriveForklift : MonoBehaviour
 {
     public CameraManipulation forkliftCams = null;
     public float direction;
-    //public bool collision = true;
 
     public SceneNode forkliftBase;
     public GameObject frame;
@@ -14,11 +13,11 @@ public class DriveForklift : MonoBehaviour
     public SceneNode forksSceneNode;
     public GameObject leftFork;
     public GameObject rightFork;
-    public bool leftCollide, rightCollide;
-
-    public GameObject box;
 
     public TheWorld world = null;
+
+    public Vector3 startingPosition;
+    public bool collision;
 
     void Start()
     {
@@ -28,7 +27,7 @@ public class DriveForklift : MonoBehaviour
 
     void Update()
     {
-        checkCollision();
+        startingPosition = forkliftBase.transform.localPosition;
         if (forkliftBase.transform.right.x < 0)
         {
             direction = -1;
@@ -53,73 +52,15 @@ public class DriveForklift : MonoBehaviour
         {
             forkliftBase.transform.localPosition += forkliftBase.transform.right * 0.01f * direction;
         }
-
+        checkCollision();
+        if (collision)
+        {
+            forkliftBase.transform.localPosition = startingPosition;
+            collision = false;
+        }
         forkliftCams.UpdateCameras();
     }
 
-    /*public void checkCollision()
-    {
-        Matrix4x4 parentMatrix = forksSceneNode.getCombinedMatrix();
-        Vector3 position = parentMatrix.GetColumn(3);
-        Mesh leftMesh = leftFork.GetComponent<MeshFilter>().mesh;
-        Vector3[] leftVertices = leftMesh.vertices;
-        Mesh rightMesh = rightFork.GetComponent<MeshFilter>().mesh;
-        Vector3[] rightVertices = rightMesh.vertices;
-        Collider collider = box.transform.GetComponent<Collider>();
-        for (int i = 0; i < rightVertices.Length; i++)
-        {
-            if (collider.bounds.Contains(leftVertices[i] + position))
-            {
-                leftCollide = true;
-            } else
-            {
-                leftCollide = false;
-            }
-            if (collider.bounds.Contains(rightVertices[i] + position))
-            {
-                rightCollide = true;
-            } else
-            {
-                rightCollide = false;
-            }
-        }
-    }*/
-
-    /*public void checkCollision()
-    {
-        Matrix4x4 parentMatrix = forksSceneNode.getCombinedMatrix();
-        Vector3 position = parentMatrix.GetColumn(3);
-        Mesh leftMesh = leftFork.GetComponent<MeshFilter>().mesh;
-        Vector3[] leftVertices = leftMesh.vertices;
-        Mesh rightMesh = rightFork.GetComponent<MeshFilter>().mesh;
-        Vector3[] rightVertices = rightMesh.vertices;
-        BoxCollider collider = box.transform.GetComponent<BoxCollider>();
-        for (int i = 0; i < leftVertices.Length; i++)
-        {
-            Vector3 leftPoint = leftVertices[i] + position;
-            leftPoint = collider.transform.InverseTransformPoint(leftPoint) - collider.center;
-            Vector3 rightPoint = rightVertices[i] + position;
-            rightPoint = collider.transform.InverseTransformPoint(rightPoint) - collider.center;
-            float halfX = (collider.size.x * 0.5f);
-            float halfY = (collider.size.y * 0.5f);
-            float halfZ = (collider.size.z * 0.5f);
-            if (leftPoint.x < halfX && leftPoint.x > -halfX && leftPoint.y < halfY && leftPoint.y > -halfY && leftPoint.z < halfZ && leftPoint.z > -halfZ)
-            {
-                leftCollide = true;
-            } else
-            {
-                leftCollide = false;
-            }
-            if (rightPoint.x < halfX && rightPoint.x > -halfX && rightPoint.y < halfY && rightPoint.y > -halfY && rightPoint.z < halfZ && rightPoint.z > -halfZ)
-            {
-                rightCollide = true;
-            }
-            else
-            {
-                rightCollide = false;
-            }
-        }
-    }*/
     public void checkCollision()
     {
         ArrayList toTest = world.testCollision(transform);
@@ -141,7 +82,8 @@ public class DriveForklift : MonoBehaviour
                 if (fineCollisionBody || fineCollisionLeftFork || fineCollisionRightFork)
                 {
                     Debug.Log("Fine Collision!");
-                }
+                    collision = true;
+                } 
             }
             
         }
